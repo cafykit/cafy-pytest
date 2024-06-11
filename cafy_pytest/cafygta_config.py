bd2685109e1e4a1decc4e5dd403c4b5fdb60e612
Pashupati Nath Verma <verma2471995@gmail.com>
2024-06-11 07:39:57 +0530

    updating post api url from dev to prod

--- cafy_pytest/cafygta_config.py
+++ cafy_pytest/cafygta_config.py
@@ -1,5 +1,5 @@
 class CafyGTA_Configs:
-    GTA_URL = 'https://cafy3.cisco.com:3500/api/gta'
+    GTA_URL = 'https://cafy3.cisco.com/api/gta'
     API_KEY = 'f912594816b83760756f3cfdcb32f08bdf3f9b6fe46183323ec0aaf0e4afe25b'
     @staticmethod
     def get_gta_url():
