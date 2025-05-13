94fbe0af01c9c1d6f66ab3132c9b9a42d58f1218
pashupati123 <verma2471995@gmail.com>
2025-05-13 12:19:09 +0530

    .

--- cafy_pytest/plugin.py
+++ cafy_pytest/plugin.py
@@ -1315,7 +1315,8 @@ class EmailReport(object):
                 if (test_class not in self.analyzer_testcase.keys()) or self.analyzer_testcase.get(test_class) == 1:
                     analyzer_status = self.post_testcase_status(reg_id, testcase_name, CafyLog.debug_server)
                     if analyzer_status and analyzer_status['status'] == True and self.testcase_dict[testcase_name].status == 'passed':
-                        analyzer_status['failures'] =  'failed due to crashed'
+                        if not analyzer_status['failures']:
+                            analyzer_status['failures'] =  'failed due to analyzer'
                         self.testcase_dict[testcase_name].status = 'failed'
                     self.log.info('Analyzer Status is {}'.format(analyzer_status))
                 else:
